657bc32c73f9ba2ed36ce31a79b59064b83f0099
Ioannis Vongelis <johnyvoggelis@gmail.com>
2025-08-23 11:57:57 +0300

    Create Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h1>Welcome to Little Lemon</h1>
+      <p>This is the main content area of the website</p>
+    </main>
+  );
+}
+
+export default Main;
