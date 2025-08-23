56c5378d8770714fb581f72d37195f168daf08e8
Ioannis Vongelis <johnyvoggelis@gmail.com>
2025-08-23 11:49:33 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,11 @@
+import logo from  '../logo.svg';
 
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Little Lemon Logo" className="logo" />
+    </header>
+  );
+}
+
+export default Header;
