3296c42c373913c8070a437bb23f81439e51c49b
Ιωάννης <johnyvoggelis@gmail.com>
2025-08-24 11:15:02 +0300

    Changes to Nav.js + Re-write the Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,56 @@
+import { Routes, Route } from "react-router-dom";
+
+
+function Homepage() {
+  return (
+    <>
+      <h1>Welcome to the homepage</h1>
+      <p>This is the homepage</p>
+    </>
+  );
+}
+
+function AboutPage() {
+  return (
+    <h1>About Little Lemon</h1>
+  );
+}
+
+function MenuPage() {
+  return (
+    <h1>Our Menu</h1>
+  );
+}
+
+function ReservationPage() {
+  return (
+    <h1>Reservations</h1>
+  );
+}
+
+function OrderPage() {
+  return (
+    <h1>Order Online</h1>
+  );
+}
+
+function LoginPage() {
+  return (
+    <h1>Login</h1>
+  );
+}
+
 function Main() {
   return (
     <main>
-      <h1>Welcome to Little Lemon</h1>
-      <p>This is the main content area of the website</p>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/reservations" element={<ReservationPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </main>
   );
 }
