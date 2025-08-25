9273e35a097a7cdba1ff9f417d945d0cba4ca61e
Ιωάννης <johnyvoggelis@gmail.com>
2025-08-25 19:48:50 +0300

    Chnages to Main.js and BookingForm.jswith use of the useReducer hook

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import BookingPage from "./BookingPage";
+import { useReducer } from "react";
 
+
+function initializeTimes() {
+  return ["17:00", "18:00", "19:00", "20:00", "21:00"];
+}
+
+function updateTimes(state, action) {
+  switch (action.type) {
+    case "update_date":
+      //For now, always return the same times
+      return initializeTimes();
+      default:
+        return state;
+  }
+}
 
 function Homepage() {
   return (
@@ -36,13 +51,15 @@ function LoginPage() {
 }
 
 function Main() {
+  const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
+
   return (
     <main>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/menu" element={<MenuPage />} />
-        <Route path="/booking" element={<BookingPage />} />
+        <Route path="/booking" element={<BookingPage availableTimes={availableTimes} dispatch={dispatch} />} />
         <Route path="/order" element={<OrderPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
