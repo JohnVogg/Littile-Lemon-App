e182b0f6edd2748aecbebda1d3db9ea546c8ca87
Ιωάννης <johnyvoggelis@gmail.com>
2025-08-26 18:59:47 +0300

    Fix the tests folder in oder to be compatioble with the API call

--- src/tests/MainTest.js
+++ src/tests/MainTest.js
@@ -1,12 +1,25 @@
 import { initializeTimes, updateTimes } from "../components/Main";
+import { fetchAPI } from "../../public/index.html";
+
+jest.mock("../../public/index.html");
 
 test("initialize returns the the correct initial available times", () => {
-    const times = initializeTimes();
-    expect(times).toEqual(["17:00", "18:00", "19:00", "20:00", "21:00"]);
+    const mockTimes = ["17:00", "18:00", "19:00"];
+    fetchAPI.mockReturnValue(mockTimes);
+
+    const result = initializeTimes();
+    expect(fetchAPI).toEqual(mockTimes);
+    expect(fetchAPI).toHaveBeenCalled();
 });
 
-test("updateTimes returns the same state provided", () => {
-    const initalState = ["17:00", "18;00"];
-    const newState = updateTimes(initalState, { type: "update", payload: "2023-08-20"});
-    expect(newState).toEqual(initialState);
+test("updateTimes returns available times for a given date", () => {
+    const mockTimes = ["20:00", "21:00"];
+    const selectedDate = new Date("2025-08-20");
+    
+    fetchAPI.mockReturnValue(mockTimes);
+  
+    const result = updateTimes([], { type: "update_times", payload: selectedDate });
+    
+    expect(result).toEqual(mockTimes);
+    expect(fetchAPI).toHaveBeenCalledWith(selectedDate);
 });
